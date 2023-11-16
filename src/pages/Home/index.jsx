d521d6b08e795664dbdcca9f6a9495652f53e1f3
D1ogooo <diiogocosta11@gmail.com>
2023-11-16 12:07:24 -0300

    inicio

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,20 @@
+import { Header, Logo } from './style'
+import { Container, HomeLink, PedidosLink } from './style'
+
+function Home (){
+  return(
+   <>
+   <Header>
+    <Logo></Logo>
+    <ul>
+     <li>
+     HomeLink
+     </li>
+     <li>
+     PedidosLink
+     </li>
+    </ul>
+   </Header>
+   </>
+  )
+}
---
+++ src/pages/Home/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+ width: 90%;
+ margin: 0 auto;
+`
+
+export const Header = styled.header`
+ 
+`
+
+export const Logo = styled.div`
+ 
+`
+
+export const HomeLink = styled(Link)`
+ 
+`
